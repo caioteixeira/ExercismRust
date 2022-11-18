928932f26914da6758a37434c4bac7095f46a7cf
Caio Teixeira <caioteixeira.dev@gmail>
2022-11-18 09:22:47 -0300

    maganize-cutout with or_insert

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -7,29 +7,19 @@ use std::collections::{hash_map, HashMap};
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
     let mut magazine_words = HashMap::new();
 
-    for entry in magazine {
-        match magazine_words.get(entry) {
-            Some(count) => {
-                magazine_words.insert(entry, count + 1);
-            }
-            None => {
-                magazine_words.insert(entry, 1);
-            }
-        }
+    for magazine_word in magazine {
+        let entry = magazine_words.entry(magazine_word).or_insert(0);
+        *entry += 1;
     }
 
-    for entry in note {
-        match magazine_words.get(entry) {
-            Some(count) => {
-                if (count <= &0) {
-                    return false;
-                }
-                magazine_words.insert(entry, count - 1);
-            }
-            None => {
-                return false;
-            }
+    for note_word in note {
+        let entry = magazine_words.entry(note_word).or_insert(0);
+
+        if *entry == 0 {
+            return false;
         }
+
+        *entry -= 1;
     }
 
     true

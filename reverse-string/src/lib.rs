e583d69e1a167e84644a1d60790cbb7170e5cf29
Caio Teixeira <caioteixeira.dev@gmail>
2022-11-21 19:28:27 -0300

    reverse string with unicode segmentation

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,12 @@
+#[cfg(feature = "grapheme")]
+use unicode_segmentation::UnicodeSegmentation;
+
+#[cfg(not(feature = "grapheme"))]
 pub fn reverse(input: &str) -> String {
     input.rsplit("").collect()
+}
+
+#[cfg(feature = "grapheme")]
+pub fn reverse(input: &str) -> String {
+    input.graphemes(true).rev().collect::<String>()
 }

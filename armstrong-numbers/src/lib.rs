d0b67f2a9c1f65ce8c81598ba6f27cc0b1ef20e9
Caio Teixeira <caioteixeira.dev@gmail>
2022-11-24 13:32:45 -0300

    Armstrong numbers solution

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,25 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let num_digits = count_digits(num);
+
+    let mut remainder = num;
+    let mut sum = 0;
+
+    while remainder > 0 {
+        sum += (remainder % 10).pow(num_digits);
+        remainder /= 10;
+    }
+
+    sum == num
+}
+
+fn count_digits(num: u32) -> u32 {
+    let mut num_digits = 1;
+    let mut remainder = num;
+
+    while remainder > 10 {
+        remainder /= 10;
+        num_digits += 1;
+    }
+
+    num_digits
 }

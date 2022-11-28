40bc07d292a3f1998a02c3a592ee2b85dfec25cf
Caio Teixeira <caioteixeira.dev@gmail>
2022-11-28 19:36:03 -0300

    High scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        let mut ordered_scores = self.scores.to_vec();
+        ordered_scores.sort();
+        ordered_scores.last().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut ordered_scores = self.scores.to_vec();
+        ordered_scores.sort();
+        ordered_scores.reverse();
+        ordered_scores.truncate(3);
+        ordered_scores
+    }
+}

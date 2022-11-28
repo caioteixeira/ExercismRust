a51e333848adc559e2c8e75260cc4bf3eaef7d26
Caio Teixeira <caioteixeira.dev@gmail>
2022-11-28 19:40:24 -0300

    Use max for high scores

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -17,9 +17,7 @@ impl<'a> HighScores<'a> {
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        let mut ordered_scores = self.scores.to_vec();
-        ordered_scores.sort();
-        ordered_scores.last().copied()
+        self.scores.iter().max().copied()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {

b8ca4e5e56121f99c6516047c5709bd58fa92f35
Caio Teixeira <caioteixeira.dev@gmail.com>
2023-09-26 09:20:30 -0300

    Improve solution for space age with associated consts

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -2,19 +2,19 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration {
-    seconds: u64
-}
+pub struct Duration(f64);
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        Duration { seconds: s }
+        Duration(s as f64)
     }
 }
 
 pub trait Planet {
+    const ORBIT_DURATION : f64;
+
     fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+        (d.0 / 31557600.0) / Self::ORBIT_DURATION
     }
 }
 
@@ -28,49 +28,32 @@ pub struct Uranus;
 pub struct Neptune;
 
 impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_years = (d.seconds as f64) / 31557600f64;
-        earth_years / 0.2408467f64
-    }
+    const ORBIT_DURATION : f64 = 0.2408467;
 }
 impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_years = (d.seconds as f64) / 31557600f64;
-        earth_years / 0.61519726f64
-    }
+    const ORBIT_DURATION : f64 = 0.61519726;
 }
+
 impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        (d.seconds as f64) / 31557600f64
-    }
+    const ORBIT_DURATION : f64 = 1.0;
 }
+
 impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_years = (d.seconds as f64) / 31557600f64;
-        earth_years / 1.8808158f64
-    }
+    const ORBIT_DURATION : f64 = 1.8808158;
 }
+
 impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_years = (d.seconds as f64) / 31557600f64;
-        earth_years / 11.862615f64
-    }
+    const ORBIT_DURATION : f64 = 11.862615;
 }
+
 impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_years = (d.seconds as f64) / 31557600f64;
-        earth_years / 29.447498f64
-    }
+    const ORBIT_DURATION : f64 = 29.447498;
+
 }
 impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_years = (d.seconds as f64) / 31557600f64;
-        earth_years / 84.016846f64
-    }
+    const ORBIT_DURATION : f64 = 84.016846;
 }
+
 impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        let earth_years = (d.seconds as f64) / 31557600f64;
-        earth_years / 164.79132f64
-    }
+    const ORBIT_DURATION : f64 = 164.79132;
 }
